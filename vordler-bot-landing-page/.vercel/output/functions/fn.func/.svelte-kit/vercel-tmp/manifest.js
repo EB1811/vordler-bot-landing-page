efca20c24ebe09fa10bc503f005bfcf32d7c2a61
Emmanuils Borovikovs <emmanuilsborovikovs@outlook.com>
2023-08-12 12:07:17 +0100

    built

--- vordler-bot-landing-page/.vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
+++ vordler-bot-landing-page/.vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
@@ -7,10 +7,10 @@ function __memo(fn) {
 return {
 	appDir: "_app",
 	appPath: "_app",
-	assets: new Set(["abstract3.jpg","favicon-32x32.png"]),
-	mimeTypes: {".jpg":"image/jpeg",".png":"image/png"},
+	assets: new Set(["abstract3.jpg","favicon-32x32.png","favicon.ico"]),
+	mimeTypes: {".jpg":"image/jpeg",".png":"image/png",".ico":"image/vnd.microsoft.icon"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.74873226.js","app":"_app/immutable/entry/app.9e752770.js","imports":["_app/immutable/entry/start.74873226.js","_app/immutable/chunks/scheduler.389d799c.js","_app/immutable/chunks/singletons.bdcf74eb.js","_app/immutable/entry/app.9e752770.js","_app/immutable/chunks/scheduler.389d799c.js","_app/immutable/chunks/index.d50ceac4.js"],"stylesheets":[],"fonts":[]},
+		client: {"start":"_app/immutable/entry/start.ae280514.js","app":"_app/immutable/entry/app.77b2499a.js","imports":["_app/immutable/entry/start.ae280514.js","_app/immutable/chunks/scheduler.389d799c.js","_app/immutable/chunks/singletons.f075780c.js","_app/immutable/entry/app.77b2499a.js","_app/immutable/chunks/scheduler.389d799c.js","_app/immutable/chunks/index.d50ceac4.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			__memo(() => import('../output/server/nodes/0.js')),
 			__memo(() => import('../output/server/nodes/1.js')),
