84efebfd18025b130e1d23bbf462d6eac1bc3998
Emmanuils Borovikovs <emmanuilsborovikovs@outlook.com>
2023-08-24 15:17:30 +0100

    robots.txt

--- vordler-bot-landing-page/.vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++ vordler-bot-landing-page/.vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.b9b79087.js","_app/immutable/chunks/scheduler.b108d059.js","_app/immutable/chunks/index.673e915c.js","_app/immutable/chunks/singletons.3e1dfcb9.js"];
+export const imports = ["_app/immutable/nodes/1.a2b820b4.js","_app/immutable/chunks/scheduler.b108d059.js","_app/immutable/chunks/index.673e915c.js","_app/immutable/chunks/singletons.5e4b9680.js"];
 export const stylesheets = [];
 export const fonts = [];
--- vordler-bot-landing-page/.vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
+++ vordler-bot-landing-page/.vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
@@ -7,10 +7,10 @@ function __memo(fn) {
 return {
 	appDir: "_app",
 	appPath: "_app",
-	assets: new Set(["abstract3.jpg","favicon-32x32.png","favicon.ico"]),
-	mimeTypes: {".jpg":"image/jpeg",".png":"image/png",".ico":"image/vnd.microsoft.icon"},
+	assets: new Set(["abstract3.jpg","favicon-32x32.png","favicon.ico","robots.txt"]),
+	mimeTypes: {".jpg":"image/jpeg",".png":"image/png",".ico":"image/vnd.microsoft.icon",".txt":"text/plain"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.cfe45901.js","app":"_app/immutable/entry/app.1c89cd0b.js","imports":["_app/immutable/entry/start.cfe45901.js","_app/immutable/chunks/scheduler.b108d059.js","_app/immutable/chunks/singletons.3e1dfcb9.js","_app/immutable/entry/app.1c89cd0b.js","_app/immutable/chunks/scheduler.b108d059.js","_app/immutable/chunks/index.673e915c.js"],"stylesheets":[],"fonts":[]},
+		client: {"start":"_app/immutable/entry/start.e198675e.js","app":"_app/immutable/entry/app.101993cb.js","imports":["_app/immutable/entry/start.e198675e.js","_app/immutable/chunks/scheduler.b108d059.js","_app/immutable/chunks/singletons.5e4b9680.js","_app/immutable/entry/app.101993cb.js","_app/immutable/chunks/scheduler.b108d059.js","_app/immutable/chunks/index.673e915c.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			__memo(() => import('../output/server/nodes/0.js')),
 			__memo(() => import('../output/server/nodes/1.js'))
